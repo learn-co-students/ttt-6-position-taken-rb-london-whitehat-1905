c605c60c5f14cab325cf825d0448baf7497833e9
bmordan <bernardmordan@gmail.com>
2019-04-17 14:38:43 +0100

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,7 @@
 # code your #position_taken? method here!
+
+def position_taken?(board, index)
+    position = board[index]
+    if position == "X" || position == "O" then return true end
+    ![" ", "", nil].any? {|empty| empty == position}
+end
